8cf6ae3f73eca8fae4b9997e78ff27a7564c14fa
AminDaoudi <89942514+AminDaoudi@users.noreply.github.com>
2024-09-15 16:11:08 -0400

    Update portscanner2.sh

--- portscanner2.sh
+++ portscanner2.sh
@@ -17,8 +17,8 @@ for ((ip=1; ip<=254; ip++)); do
   ipaddr="$network.$ip"
 
 
-  timeout 1 bash -c "echo > /dev/tcp/$ipaddr/$port" 2>/dev/null && echo "$ipaddr,$port" >> s>
-done
+  timeout 1 bash -c "echo > /dev/tcp/$ipaddr/$port" 2>/dev/null && echo "$ipaddr,$port" >> scan_results.csv
+do
 
 
 echo "Scan complete. results saved in scan_results.csv"
